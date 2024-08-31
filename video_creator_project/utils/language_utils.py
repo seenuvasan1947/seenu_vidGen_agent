c9daaa1a4755df7671a8578a265f3b3eebc19ba5
seenuvasan1947 <seenuthiruvpm@gmail.com>
2024-08-31 09:19:09 +0530

    lang not found resolved

--- video_creator_project/utils/language_utils.py
+++ video_creator_project/utils/language_utils.py
@@ -1,48 +1,11 @@
-from deep_translator import GoogleTranslator
-from gtts import gTTS
-from config import SUPPORTED_LANGUAGES
-
-def initialize_supported_languages():
-    global SUPPORTED_LANGUAGES
-    SUPPORTED_LANGUAGES.update(
-        set(GoogleTranslator().get_supported_languages()) & set(gTTS.lang.tts_langs().keys())
-    )
-
-def get_supported_languages():
-    return sorted(list(SUPPORTED_LANGUAGES))
-
-def display_language_options():
-    languages = get_supported_languages()
-    print("\nAvailable languages for translation and text-to-speech:")
-    for i, lang in enumerate(languages, 1):
-        print(f"{i}. {lang.capitalize()}")
-    return languages
-
-def get_language_choice(prompt):
-    print(prompt)
-    languages = display_language_options()
-    while True:
-        try:
-            lang_index = int(input("Enter the number of the desired language: ")) - 1
-            if 0 <= lang_index < len(languages):
-                return languages[lang_index]
-            else:
-                print("Invalid choice. Please try again.")
-        except ValueError:
-            print("Please enter a valid number.")
-
-initialize_supported_languages()
-
-
 # from deep_translator import GoogleTranslator
 # from gtts import gTTS
-# import gtts.lang
 # from config import SUPPORTED_LANGUAGES
 
 # def initialize_supported_languages():
 #     global SUPPORTED_LANGUAGES
-#     SUPPORTED_LANGUAGES = list(
-#         set(GoogleTranslator().get_supported_languages()) & set(gtts.lang.tts_langs().keys())
+#     SUPPORTED_LANGUAGES.update(
+#         set(GoogleTranslator().get_supported_languages()) & set(gTTS.lang.tts_langs().keys())
 #     )
 
 # def get_supported_languages():
@@ -69,3 +32,40 @@ initialize_supported_languages()
 #             print("Please enter a valid number.")
 
 # initialize_supported_languages()
+
+
+from deep_translator import GoogleTranslator
+from gtts import gTTS
+import gtts.lang
+from config import SUPPORTED_LANGUAGES
+
+def initialize_supported_languages():
+    global SUPPORTED_LANGUAGES
+    SUPPORTED_LANGUAGES = list(
+        set(GoogleTranslator().get_supported_languages()) & set(gtts.lang.tts_langs().keys())
+    )
+
+def get_supported_languages():
+    return sorted(list(SUPPORTED_LANGUAGES))
+
+def display_language_options():
+    languages = get_supported_languages()
+    print("\nAvailable languages for translation and text-to-speech:")
+    for i, lang in enumerate(languages, 1):
+        print(f"{i}. {lang.capitalize()}")
+    return languages
+
+def get_language_choice(prompt):
+    print(prompt)
+    languages = display_language_options()
+    while True:
+        try:
+            lang_index = int(input("Enter the number of the desired language: ")) - 1
+            if 0 <= lang_index < len(languages):
+                return languages[lang_index]
+            else:
+                print("Invalid choice. Please try again.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+initialize_supported_languages()
